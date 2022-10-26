dd502c5410733673d5be8a5d73f1fe7211a87db0
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-10-26 17:01:34 +0600

    course added

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-const categories = require('./Data/course.json');
+const categories = require("./Data/course.json");
+const courses = require("./Data/details.json");
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.get("/categories", (req, res) => {
-    res.send(categories);
+  res.send(categories);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((c) => c.id == id);
+  res.send(course);
 });
 
 app.listen(port, () => {
