17474f8c9b0e5a69f603dd03075ece2c5bbcdff9
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-10-28 21:56:51 +0600

    all done

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
 
-
 const courses = require("./Data/course.json");
-// const details = require("./Data/details.json");
+const details = require("./Data/details.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -17,14 +16,25 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
-// app.get("/courses/:id", (req, res) => {
-//   const id = req.params.id;
-//   const course = details.find((c) => c.id == id);
-//   if (!course) {
-//     res.send('course nei')
-//   }
-//   res.send(course);
-// });
+app.get("/allCourses", (req, res) => {
+  res.send(details);
+});
+
+app.get("/allCourses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = details.find((c) => c._id === id);
+  res.send(selectedCourse);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "00") {
+    res.send(details);
+  } else {
+    const selectedCategory = details.filter((c) => c.course_id === id);
+    res.send(selectedCategory);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
