b7a99b33847b29593b68ded1ef375524220d8dcc
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-10-28 07:28:24 +0600

    details added

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const port = process.env.PORT || 5000;
 app.use(cors());
 
-const port = process.env.PORT || 5000;
 
 const courses = require("./Data/course.json");
-const details = require("./Data/details.json");
+// const details = require("./Data/details.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -17,14 +17,14 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
-app.get("/courses/:id", (req, res) => {
-  const id = req.params.id;
-  const course = details.find((c) => c.id == id);
-  if (!course) {
-    res.send('course nei')
-  }
-  res.send(course);
-});
+// app.get("/courses/:id", (req, res) => {
+//   const id = req.params.id;
+//   const course = details.find((c) => c.id == id);
+//   if (!course) {
+//     res.send('course nei')
+//   }
+//   res.send(course);
+// });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
